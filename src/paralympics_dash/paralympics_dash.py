9104a88fdd160b414e2780f7ad9f8c7401554055
Fishman2k16 <85204835+Fishman2k16@users.noreply.github.com>
2024-02-23 15:19:19 +0000

    Update paralympics_dash.py

--- src/paralympics_dash/paralympics_dash.py
+++ src/paralympics_dash/paralympics_dash.py
@@ -1,6 +1,12 @@
 from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+# Variable that contains the external_stylesheet to use, in this case Bootstrap styling from dash bootstrap components (dbc)
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+# Pass the stylesheet variable to the Dash app constructor
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
 
 app.layout = html.Div([
     html.Div(children='Hello World')
